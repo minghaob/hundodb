e3a750ca308dc398da2bdbaea8576aba77f7568c
minghaob <131937111+minghaob@users.noreply.github.com>
2024-10-03 15:56:35 +0200

    Implement export .lss from a run. Add split_timing doc page

---
+++ docs/split_timing.md
@@ -0,0 +1,16 @@
+## Split timings
+When exporting LiveSplit (*.lss) from a run in HundoDB, it is possible to also include the split times in the exported file.
+
+However, be aware that the split timing might be different from what the user originally uses.
+
+If the split times are to be used for comparison (either when practicing or with another run), make sure that you also split as follows:
+| Type   | Split timing |
+|---|---|
+| Shrines   | When **screen turns black** to start the loading sequence when leaving shrine  |
+| Towers  | When dialog box says **"Sheikah Tower activated."** after Link puts down the Sheikah Slate  |
+| Divine Beasts  | When **screen turns white** after defeating Blight  |
+| Enter Divine Beasts | When dialog box says **"Travel Gate registered to map."** after Link scans the Sheikah Slate |
+| Paraglider | When the **"Paraglider"** item text box is displayed  |
+| Thunder Helm | When the **"Thunder Helm"** item text box is displayed  |
+| Warps | When confirming **"Travel"** on the map screen  |
+| Koroks |  When the **"Korok Seed"** item text box is displayed  |
--- js/export_panel.js
+++ js/export_panel.js
@@ -20,11 +20,6 @@ let exportSplitOptions = [
 		checked: true,
 	},
 	{
-		name: 'Warps',
-		format: 'Warp {name}',
-		checked: true,
-	},
-	{
 		name: 'Paraglider',
 		format: 'Paraglider',
 		checked: true,
@@ -33,6 +28,11 @@ let exportSplitOptions = [
 		name: 'Thunder Helm',
 		format: 'Thunder Helm',
 		checked: true,
+	},
+	{
+		name: 'Warps',
+		format: 'Warp {name}',
+		checked: false,
 	},
 	{
 		name: 'Koroks',
@@ -76,6 +76,7 @@ function initExportPanel() {
 	}
 
 	document.getElementById('export_select').onchange = () => updateExportLSSButton();
+	document.getElementById('export_splits').onclick = () => exportLSS();
 }
 
 function updateExportLSSButton() {
@@ -86,7 +87,6 @@ function updateExportLSSButton() {
 		return;
 	}
 
-	let options = document.getElementById('export_split_options').querySelector('tbody').rows;
 	let enable = false;
 	for (let option of exportSplitOptions) {
 		let id = 'export_split_' + option.name;
@@ -109,4 +109,219 @@ function syncDBToExportPane() {
 	for (const runId of runIds) {
 		exportNode.append(new Option(runId, runId));
 	}
+}
+
+function ResolveFormatString(format, numKorok, name) {
+	return format.replace('{numKorok}', numKorok).replace('{name}', name);
+}
+
+function exportLSS() {
+	let exportRunUID = document.getElementById("export_select").value;
+
+	let formats = {};
+	for (let option of exportSplitOptions) {
+		let id = 'export_split_' + option.name;
+		let id2 = id + '_format';
+		if (document.getElementById(id).checked) {
+			formats[option.name] = document.getElementById(id2).value;
+		}
+	}
+
+	let run = g_runs[exportRunUID];
+	if (!run)
+		return;
+
+	let splits = [];
+	let numKorok = 0;
+	const events = run.events;
+	for (let evtIdx = 0; evtIdx < events.length; evtIdx++) {
+		let event = events[evtIdx];
+		if (event.type == 'Shrine') {
+			let format = formats['Shrines'];
+			if (format) {
+				splits.push([
+					ResolveFormatString(format, numKorok, event.label.slice(0, -7)),
+					event.segments[1][0],
+				]);
+			}
+		}
+		else if (event.type == 'Tower Activation') {
+			let format = formats['Towers'];
+			if (format) {
+				splits.push([
+					ResolveFormatString(format, numKorok, event.label),
+					event.frame[0],
+				]);
+			}
+		}
+		else if (event.type == 'Medoh' || event.type == 'Naboris' ||event.type == 'Ruta' ||event.type == 'Rudania') {
+			let format = formats['Enter Divine Beasts'];
+			if (format) {
+				splits.push([
+					ResolveFormatString(format, numKorok, event.label),
+					event.frame[0],
+				]);
+			}
+			format = formats['Divine Beasts'];
+			if (format) {
+				splits.push([
+					ResolveFormatString(format, numKorok, event.label),
+					event.segments[6][0],
+				]);
+			}
+		}
+		else if (event.type == 'Paraglider') {
+			let format = formats['Paraglider'];
+			if (format) {
+				splits.push([
+					ResolveFormatString(format, numKorok, event.label),
+					event.frame[0],
+				]);
+			}
+		}
+		else if (event.type == 'Thunder Helm') {
+			let format = formats['Thunder Helm'];
+			if (format) {
+				splits.push([
+					ResolveFormatString(format, numKorok, event.label),
+					event.frame[0],
+				]);
+			}
+		}
+		else if (event.type == 'Warp') {
+			let format = formats['Warps'];
+			if (format) {
+				let name = event.label;
+				if (name.endsWith(' (Tamed)'))
+					name = name.slice(0, -8);
+				if (name.endsWith(' Shrine'))
+					name = name.slice(0, -7);
+				splits.push([
+					ResolveFormatString(format, numKorok, name),
+					event.frame[0],
+				]);
+			}
+		}
+		else if (event.type == 'Korok Seed') {
+			numKorok++;
+			let format = formats['Koroks'];
+			if (format) {
+				splits.push([
+					ResolveFormatString(format, numKorok, event.label),
+					event.frame[0],
+				]);
+			}
+		}
+		else if (event.type == 'GG') {
+			splits.push([
+				'100%',
+				event.frame[0],
+			]);
+		}
+	}
+
+	let lss = createLSS(splits, document.getElementById('export_split_time').checked);
+	downloadFile(lss, exportRunUID + '.lss');
+}
+
+function createLSS(splits, includeTime) {
+	let content =
+`<?xml version="1.0" encoding="UTF-8"?>
+<Run version="1.7.0">
+  <GameIcon />
+  <GameName>The Legend of Zelda: Breath of the Wild</GameName>
+  <CategoryName>100%</CategoryName>
+  <LayoutPath>
+  </LayoutPath>
+  <Metadata>
+    <Run id="" />
+    <Platform usesEmulator="False">
+    </Platform>
+    <Region>
+    </Region>
+    <Variables />
+  </Metadata>
+  <Offset>-00:00:01</Offset>
+  <AttemptCount>` + (includeTime ? 1 : 0) + `</AttemptCount>
+  <AttemptHistory>
+`
+	if (includeTime) {
+		content += 
+`    <Attempt id="1">
+      <RealTime>` + frameIdxToTime(splits.at(-1)[1]) + `</RealTime>
+    </Attempt>
+`;
+	}
+	content +=
+`  </AttemptHistory>
+  <Segments>
+`;
+
+	if (includeTime) {
+		let last = 0;
+		for (let split of splits) {
+			content +=
+`    <Segment>
+      <Name>` + split[0] + `</Name>
+      <Icon />
+      <SplitTimes>
+        <SplitTime name="Personal Best">
+          <RealTime>` + frameIdxToTime(split[1]) + `</RealTime>
+        </SplitTime>
+      </SplitTimes>
+      <BestSegmentTime>
+        <RealTime>` + frameIdxToTime(split[1] - last) + `</RealTime>
+      </BestSegmentTime>
+      <SegmentHistory>
+        <Time id="1">
+          <RealTime>` + frameIdxToTime(split[1] - last) + `</RealTime>
+        </Time>
+      </SegmentHistory>
+    </Segment>
+`;
+			last = split[1];
+		}
+	}
+	else {
+		for (let split of splits) {
+			content +=
+`    <Segment>
+      <Name>` + split[0] + `</Name>
+      <Icon />
+      <SplitTimes>
+        <SplitTime name="Personal Best" />
+      </SplitTimes>
+      <BestSegmentTime />
+      <SegmentHistory />
+    </Segment>
+`;
+		}
+	}
+
+	content +=
+`  </Segments>
+  <AutoSplitterSettings />
+</Run>`;
+
+	return content;
+}
+
+function downloadFile(content, name) {
+	// Create a Blob from the file content
+	const blob = new Blob([content], { type: 'text/plain' });
+
+	// Create an anchor element
+	const a = document.createElement('a');
+
+	// Create a URL for the Blob and set it as the href attribute
+	a.href = URL.createObjectURL(blob);
+
+	// Set the desired file name for download
+	a.download = name;
+
+	// Programmatically click the anchor to trigger the download
+	a.click();
+
+	// Release the object URL after the download to avoid memory leaks
+	URL.revokeObjectURL(a.href);
 }
